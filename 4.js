eb47339e0e29afc97a314c6d09e3365852c34963
minhduc1809 <119800775+minhduc1809@users.noreply.github.com>
2025-03-14 08:07:20 +0700

    Update 4.js

--- 4.js
+++ 4.js
@@ -3,7 +3,7 @@ let y = "19";
 
 console.log(x == y); 
 console.log(x === y); 
-console.log(x > 5);  
-console.log(x < 20);  
-console.log(x >= 10);
-console.log(x <= 5);
+console.log(x > y);  
+console.log(x < y);  
+console.log(x >= y);
+console.log(x <= y);
